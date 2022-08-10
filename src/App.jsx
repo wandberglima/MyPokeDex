debb149f7656617e1657d5c5bd21db513810d73c
Wandberg de Lima <wandberg@gmail.com>
2022-08-10 00:32:24 -0300

    Criação das pastas e arquivos base da Pokedex.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import './Styles/style.css';
+// import {  useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <header className='cabacalho'>
+       <h1>My Pokedex</h1>
+      </header>
+      <section className='lado-direito'>
+
+      </section>
+      <section className='lado-esquerdo'>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
