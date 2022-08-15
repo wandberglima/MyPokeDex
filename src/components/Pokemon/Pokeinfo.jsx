35aefa925b50bcc6aae775f1ae48e66f22cb4dfe
Wandberg de Lima <wandberg@gmail.com>
2022-08-15 04:04:28 -0300

    adicionando a barra de status

--- src/components/Pokemon/Pokeinfo.jsx
+++ src/components/Pokemon/Pokeinfo.jsx
@@ -1,70 +1,72 @@
 import React from "react";
 
 const Pokeinfo = ({ data }) => {
-
-    return (
+  return (
+    <>
+      {!data ? (
+        ""
+      ) : (
         <>
-        {
-            (!data) ? "" : (
-                <>
-{/* Add EasterEgg de qualquer pokemon tag ficticia */}
-<i className='easterEgg'> 
-    if ({data.id} == {data.name}) {
-        document.querySelector("[name=EasterEgg]").addEventListener('click', function() {
-            this.style.backgroundImage = `url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg') `;
-            // this.style.backgroundRepeat = `no-repeat`;
-            // this.style.backgroundPosition = `center`;
-            
-        })           
-        }
-</i>
+          {/* Add EasterEgg de qualquer pokemon tag ficticia */}
+          <i className="easterEgg">
+            if ({data.id} === {data.name}){" "}
+            {document
+              .querySelector("[name=EasterEgg]")
+              .addEventListener("click", function () {
+                this.style.backgroundImage = `url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg') `;
+              })}
+          </i>
+          <div className="card-img-top fundo">
+            {/* Card de informações do Pokemon */}
 
-
-                <div className="card-img-top fundo">
-{/* Card de informações do Pokemon */}
-
-                    <h1>{data.name}</h1>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt="" />
-                    <div className="abilities">
-                   
-                        {
-                            data.types.map(poke=>{
-                                console.log(poke);
-                                return(
-                                    <>
-                                     <div className="group">
-                                        <h2>{poke.type.name}</h2>
-                                    </div>
-                                    </>
-                                )
-                            })
-                        }
+            <h1>{data.name}</h1>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
+              alt=""
+            />
+            <div className="abilities">
+              {data.types.map((poke) => {
+                console.log(poke);
+                return (
+                  <>
+                    <div className="group">
+                      <h2>{poke.type.name}</h2>
                     </div>
-                    <div className="base-stat">
-                        {
-                            data.stats.map(poke=>{
-                                return(
-                                    <>
-                                    <div>
-                                        {poke.stat.name}
-                                        :
-                                        {poke.base_stat}
-                                        
-                                        {/* <div class="progress">
-                                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-                                        </div> */}
+                  </>
+                );
+              })}
+            </div>
+            <div className="base-stat">
+              {data.stats.map((poke) => {
+                if ({ poke } === {poke}) {
+                  document
+                    .querySelectorAll("[name=barra]")
+                    .addEventListener("click", function () {
+                      this.style.backgroundColor = `blue`;
+                      // this.style.width = `{poke.base_stat}`;
+                      this.sttyle.border = `1px solid black`;
+                      this.props.ariaValuemax = `{poke.base_stat}`;
+                    });
+                }
 
-                                    </div>
-                                    </>
-                                )
-                            })
-                        }
+                return (
+                  <>
+                    <div className="status">
+                      <div> {poke.stat.name}: </div>
+                      <div className="progress">
+                        <div name="barra" className="progress-bar">
+                          {poke.base_stat}
+                        </div>
+                      </div>
                     </div>
-                </div>  
-                </>
-            )
-        }
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </>
-    )
-}
-export default Pokeinfo
+      )}
+    </>
+  );
+};
+export default Pokeinfo;
