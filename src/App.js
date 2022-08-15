035068a127a28936cf3ed944bbc86b9edf3994eb
Wandberg de Lima <wandberg@gmail.com>
2022-08-15 02:27:23 -0300

    alterações realizadas e add da rota main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from './components/Main';
+import './styles/style.css'
+
+function App() {
+
+  return (
+    <>
+    {/* Exibição de todo conteúdo desenvolvido na main */}
+    <Main />
+    </>
+  );
+}
+
+export default App;
